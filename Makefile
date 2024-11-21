0ed6af27ee03cd98940fb3a4a90981f7d3b2d57e
chelseasoemitro <chelseasoemitro@gmail.com>
2024-11-20 22:42:53 -0500

    add cpp file for std::sort

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
 
 CC = gcc
 CFLAGS = -g -Wall -O3
+CXXFLAGS = 
 
 LDFLAGS =
-LDLIBS =
+LDLIBS = 
 
-sorting: sorting.o 
+sorting: sorting.o quicksort.h
 
-sorting.o: sorting.c quicksort.h
+cppsorting: cppsorting.o
+	g++ -std=c++17 -o cppsorting cppsorting.cpp
 
 
 .PHONY: clean
 clean:
-	rm -f *.o a.out core sorting
+	rm -f *.o a.out core sorting cppsorting
 
 .PHONY: all
-all: clean sorting
+all: clean sorting cppsorting
---
+++ cppsorting.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h> 
+
+#include <algorithm>
+
+
+// Avoid making changes to this function skeleton, apart from data type changes if required
+
+void sort_array(uint32_t *arr, size_t size) {
+    // Enter your logic here
+    std::sort(arr, arr + size);
+}
+
+int main() {
+    //Initialise the array
+    const uint64_t size = 100;
+    uint32_t *sorted_arr = (uint32_t *) malloc(size * sizeof(uint32_t)); // Allocate memory for the sorted array
+    for (int i = 0; i < size; i++) {
+        sorted_arr[i] = rand() % 100;
+    }
+
+    printf("{ ");
+    for (int i = 0; i < size; i++) {
+        printf("%u ", sorted_arr[i]);
+    }
+    printf("}\n");
+    printf("\n");
+
+    // Sort the copied array
+    sort_array(sorted_arr, size);
+
+    // Print the sorted array
+    printf("{ ");
+    for (int i = 0; i < size; i++) {
+        printf("%u ", sorted_arr[i]);
+    }
+    printf("}\n");
+
+    return 0;
+}
