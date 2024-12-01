7398f4b6f4a8ae237fbd5293a090d194ed65c678
Brennan Xavier McManus <bm2530@columbia.edu>
2024-12-01 02:35:28 +0000

    Clean up test script

--- run_tests.py
+++ run_tests.py
@@ -1,50 +1,39 @@
 import argparse
 from subprocess import call
+import sys
 
-def makeCommand(args=[]):
+def runCacheSim(flags, program):
     cmd = []
-    cmd.append("./cache_sim/pin/pin")
-    cmd.append("-t")
-    cmd.append("cache_sim/cache_simulator/obj-intel64/cache.so")
-    cmd.append("--")
-    cmd.append("echo") # todo pick our actual commmand here/pass as command line argument
+    cmd.append('./cache_sim/pin/pin')
+    cmd.extend(['-t', 'cache_sim/cache_simulator/obj-intel64/cache.so'])
 
-    for arg in args:
-        cmd.append(arg)
-    
-    return cmd
+    if flags.block:
+        cmd.extend(['-b', f'{flags.block}'])
 
-def runAssoc(assoc):
-    cmd = makeCommand()
+    if flags.capacity:
+        cmd.extend(['-dl1_c', f'{flags.capacity}'])
+
+    if flags.associativity:
+        cmd.extend(['-dl1_a',f'{flags.associativity}'])
+
+    # add program and all args
+    cmd.append('--')
+    cmd.extend(program if program else ['echo', 'default'])
+
+    print(' '.join(cmd))
     call(cmd)
 
-def runCap(capacity):
-    cmd = makeCommand()
-    call(cmd)
 
-def runBlock(blockSize):
-    cmd = makeCommand()
-    call(cmd)
-
-def runCacheSim(args):
-    if args.associativity:
-        runAssoc(args.associativity)
-    elif args.capacity:
-        runCap(args.capacity)
-    else:
-        runBlock(args.block)
-
-def Main():
+def main():
     parser = argparse.ArgumentParser(description='Run cache simulator for sensitivity study')
     
-    parser.add_argument('-b','--block', type=int, help="Measure and report misses for a 32KB direct- mapped cache, for the specified block size",
-                        default=16)
-    parser.add_argument('-c','--capacity', type=int, help="Measure and report misses for a direct-mapped cache with 64B blocks for the specified capacity")
-    parser.add_argument('-a','--associativity', type=int, help="Measure and report misses for a 64KB, 64B cache for the specified associativity")
+    parser.add_argument('-b', '--block', type=int, help='Measure and report misses for a 32KB direct- mapped cache, for the specified block size')
+    parser.add_argument('-c','--capacity', type=int, help='Measure and report misses for a direct-mapped cache with 64B blocks for the specified capacity')
+    parser.add_argument('-a','--associativity', type=int, help='Measure and report misses for a 64KB, 64B cache for the specified associativity')
 
-    args = parser.parse_args()
-    print(args.associativity)
-    runCacheSim(args)
+    flags, program = parser.parse_known_args()
 
-if __name__ == "__main__":
-    Main()
+    runCacheSim(flags, program)
+
+if __name__ == '__main__':
+    main()
